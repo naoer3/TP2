f0a902d6648da8958ad7efec53eb918dee9de906
Doriane Jaunault <dojono@free.fr>
2018-12-17 17:38:48 +0100

    ajout des tablerow dynamiques

--- app/src/main/java/tp2/mp/dj/tp2/MainActivity.java
+++ app/src/main/java/tp2/mp/dj/tp2/MainActivity.java
@@ -4,16 +4,26 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TableRow;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editName = null;
     private SeekBar seeknbcartes = null;
     private Button validate = null;
+    private int nb_fonds = 3;
+    private int nb_themes = 3;
+    private TableRow tableRow_fonds = null;
+    private TableRow tableRow_themes = null;
 
 
     @Override
@@ -23,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
         editName = (EditText) findViewById(R.id.name);
         seeknbcartes = (SeekBar) findViewById(R.id.seekBar);
         validate = (Button) findViewById(R.id.validate);
+        LayoutInflater inflater = getLayoutInflater();
+        tableRow_fonds = (TableRow) findViewById(R.id.choixfonds);
+        tableRow_themes = (TableRow) findViewById(R.id.choixthemes);
+        BuildChoixFonds();
+        BuildChoixThemes();
     }
 
     @Override
@@ -49,5 +64,22 @@ public class MainActivity extends AppCompatActivity {
         Integer nb_cartes = prefs.getInt("NB_CARTES",0);
         editName.setText(name_user);
         seeknbcartes.setProgress(nb_cartes);
+    }
+
+    private void BuildChoixFonds() {
+        for(int i =0;i<nb_fonds;i++){
+            ImageView image = new ImageView(this);
+            image.setLayoutParams(new TableRow.LayoutParams(0,ViewGroup.LayoutParams.WRAP_CONTENT,1));
+            // TODO
+            // A faire avec generique (ne marchait pas la fois précédente..)
+            image.setImageResource(R.drawable.chaton0);
+//            image.setImageResource(getResources().getIdentifier("drawable/fond"+i+".png",null, String.valueOf(getContext())));
+            tableRow_fonds.addView(image);
+        }
+    }
+
+    private void BuildChoixThemes() {
+        // TODO
+        // Sur le même exemple que la fonction BuildChoixFonds
     }
 }
