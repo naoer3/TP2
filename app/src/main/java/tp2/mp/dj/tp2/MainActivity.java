5e16394becb83bd07af987cd1c65e0e71af5d213
Mathilde Paris <mathilde.paris1@laposte.net>
2018-12-17 16:17:29 +0100

    creation fragment carte

--- app/src/main/java/tp2/mp/dj/tp2/MainActivity.java
+++ app/src/main/java/tp2/mp/dj/tp2/MainActivity.java
@@ -5,9 +5,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Carte carte = null;
+    private Carte carte2 = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        carte = (Carte)getSupportFragmentManager().findFragmentById(R.id.fragment);
+        carte2 = (Carte)getSupportFragmentManager().findFragmentById(R.id.fragment2);
+        carte.init("minion",0,2);
+        carte2.init("minion",0,7);
     }
 }
